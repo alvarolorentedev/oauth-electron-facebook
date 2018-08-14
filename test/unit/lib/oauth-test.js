51062a8ccacfb40df7a14dcfa43a995c2d9d329f
kanekotic <alvarojosepl@gmail.com>
2018-08-14 22:05:41 +0200

    started refactor and use correct dependencies

---
+++ test/unit/lib/oauth-test.js
@@ -0,0 +1,7 @@
+const common = require('../../../lib/oauth'),
+    faker = require('faker')
+
+describe('oauth should', () => {
+    test('allow login with electron window passed', async () => {
+    })
+})
